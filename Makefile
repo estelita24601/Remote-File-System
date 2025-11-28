78458f44def7522f223d8c66049fd9d2d53cb79a
Estelita <24834264+estelita24601@users.noreply.github.com>
2025-11-27 22:39:34 -0800

    writing unit tests for request and response objects (not passing yet)

--- Makefile
+++ Makefile
@@ -11,5 +11,9 @@ server: server/server.c
 test_args: client/arg_parser.c command.c
 	$(CC) $(CFLAGS) -DTEST_ARGS $^ -o rfs_test
 
+test_protocol: tests/protocol_test.c protocol.c command.c
+	$(CC) $(CFLAGS) $^ -o rfs_test -lm
+	./rfs_test
+
 clean:
 	rm -f rfs rfs_server rfs_test
---
+++ tests/protocol_test.c
@@ -0,0 +1,113 @@
+/**
+ * @file protocol_test.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2025-11-27
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#include "../protocol.h"
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../config.h"
+
+void PRINT_COMPARISON(const char* expected, const char* actual) {
+    printf("\texpected: %s\n", expected);
+    printf("\t  actual: %s\n", actual);
+}
+
+void PRINT_HEADER(const char* str) {
+    printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("TEST - %s\n", str);
+    printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+}
+
+void PRINT_SUBHEADER(const char* str) { printf("\n-- %s\n", str); }
+
+bool requestEquals(request_t* a, request_t* b) {
+    if (a == NULL && b == NULL) {
+        return true;
+    } else if (a == NULL || b == NULL) {
+        return false;
+    }
+
+    if (a->command != b->command) {
+        return false;
+    } else if (a->data_len != b->data_len) {
+        return false;
+    } else if (!equals(a->remote_path, b->remote_path)) {
+        return false;
+    }
+    return true;
+}
+
+void testRequestSerialization(request_t* original_obj, const char* expected_str) {
+    // test serialization
+    char* actual_str = serializeRequest(original_obj);
+    if (actual_str == NULL && expected_str != NULL) {
+        printf("FAIL: request_t -> char*\n");
+        PRINT_COMPARISON(expected_str, "NULL");
+        return;
+    } else if (!equals(actual_str, expected_str)) {
+        printf("FAIL: request_t -> char*\n");
+        PRINT_COMPARISON(expected_str, actual_str);
+        free(actual_str);
+        return;
+    }
+
+    // test de-serialization
+    request_t* actual_obj = deSerializeRequest(actual_str);
+    if (!requestEquals(original_obj, actual_obj)) {
+        printf("FAIL: char* -> request_t\n");
+    }
+
+    freeRequest(actual_obj);
+    free(actual_str);
+}
+
+void testRequestStruct() {
+    PRINT_HEADER("create a request_t object using invalid values should result in NULL request_t object");
+
+    PRINT_SUBHEADER("createRequest(NULL)");
+    request_t* test1 = createRequest(NULL);
+    assert(test1 == NULL);
+    testRequestSerialization(test1, NULL);
+
+    PRINT_SUBHEADER("createRequestFromParts()");
+    // invalid command type
+    request_t* test2 = createRequestFromParts(5, "remote.txt", 0);
+    assert(test2 == NULL);
+    testRequestSerialization(test2, NULL);
+    // NULL path string
+    request_t* test3 = createRequestFromParts(1, NULL, 0);
+    assert(test3 == NULL);
+    testRequestSerialization(test3, NULL);
+
+    PRINT_HEADER("create a request_t object using valid values");
+    // create request from a valid command
+    command_t* cmd1 = createCommandStruct(GET, "local1.txt", "remote1.txt");
+    request_t* request1 = createRequest(cmd1);
+    assert(request1 != NULL);
+    testRequestSerialization(request1, "1,0,remote1.txt");
+
+    freeCommandStruct(cmd1);
+    freeRequest(request1);
+
+    // create request from parts happy path
+    request_t* request2 = createRequestFromParts(GET, "remote2.txt", 0);
+    assert(request2 != NULL);
+    testRequestSerialization(request2, "1,0,remote2.txt");
+}
+
+void testResponseStruct() {}
+
+int main() {
+    testRequestStruct();
+    testResponseStruct();
+}
