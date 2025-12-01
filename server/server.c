9b0296779643942e1dfa97e5f6b089f6e2ad61a1
Estelita <24834264+estelita24601@users.noreply.github.com>
2025-11-30 18:16:11 -0800

    code cleanup

--- server/server.c
+++ server/server.c
@@ -99,24 +99,32 @@ response_t* handleWrite(request_t* write_request, const int socket_descriptor) {
 /**
  * @brief
  *
- * @param socket_descriptor
- * @param res
- * @return true
- * @return false
+ * @param socket_descriptor int - where to send the response
+ * @param res response_t* - what we want to send
+ * @return true - if successfully able to send the response to the client
+ * @return false - if unable to send the response to the client (will still try to send a plain string error message to
+ *the client)
  */
 bool sendResponse(const int socket_descriptor, response_t* res) {
+    if (res == NULL) {
+        char errorMessage[] = "ERROR: tried to send NULL response";
+        send(socket_descriptor, errorMessage, sizeof(errorMessage), 0);
+        return false;
+    }
+
     // serialize response object
     char* response_str = serializeResponse(res);
     if (response_str == NULL) {
-        fprintf(stderr, "ERROR: unable to serialize response object\n");
+        char errorMessage[] = "ERROR: unable to serialize response object";
+        send(socket_descriptor, errorMessage, sizeof(errorMessage), 0);
         return false;
     }
 
     // try to send to client
     int sendStatus = send(socket_descriptor, response_str, strlen(response_str), 0);
     if (sendStatus < 0) {
-        fprintf(stderr, "ERROR: unable to send response to the client\n");
-        free(response_str);
+        char errorMessage[] = "ERROR: unable to send serialized response to the client";
+        send(socket_descriptor, errorMessage, sizeof(errorMessage), 0);
         return false;
     }
 
@@ -144,12 +152,6 @@ void printServerAddress() {
 }
 
 int main(void) {
-    char server_msg_buffer[8196], client_msg_buffer[8196];
-
-    // Clean buffers:
-    memset(server_msg_buffer, '\0', sizeof(server_msg_buffer));
-    memset(client_msg_buffer, '\0', sizeof(client_msg_buffer));
-
     // Create socket:
     int socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_descriptor < 0) {
@@ -161,8 +163,6 @@ int main(void) {
     // set up the struct for the server address
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-
-    // use server port number and ip address defined in config.h
     server_address.sin_port = htons(PORT);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
@@ -172,27 +172,28 @@ int main(void) {
         printf("Couldn't bind to the port\n");
         return -1;
     }
-    debug("Done with binding\n");
+    printf("Done with binding\n");
 
     // print out ip address a client could use to connect to this server
     printServerAddress();
 
     socklen_t client_size;
     struct sockaddr_in client_addr;
+
+    // keep going until server process is terminated
     while (true) {
         // Listen for clients:
         int listenStatus = listen(socket_descriptor, 1);
         if (listenStatus < 0) {
-            printf("Error while listening\n");
+            fprintf(stderr, "Error while listening\n");
             close(socket_descriptor);
             return -1;
         }
-        debug("\nListening for incoming connections.....\n");
+        printf("\nListening for incoming connections.....\n");
 
         // Accept an incoming connection:
         socklen_t client_size = sizeof(client_addr);
         struct sockaddr* client_address = (struct sockaddr*) &client_addr;
-
         int client_socket_descriptor = accept(socket_descriptor, client_address, &client_size);
         if (client_socket_descriptor < 0) {
             printf("Can't accept\n");
@@ -200,8 +201,8 @@ int main(void) {
             close(client_socket_descriptor);
             return -1;
         }
-        debug("Client connected at IP: %s and port: %i\n", inet_ntoa(client_addr.sin_addr),
-              ntohs(client_addr.sin_port));
+        printf("Client connected at IP: %s and port: %i\n", inet_ntoa(client_addr.sin_addr),
+               ntohs(client_addr.sin_port));
 
         // Receive client's message:
         request_t* client_req = receiveRequest(client_socket_descriptor);
@@ -216,24 +217,30 @@ int main(void) {
             continue;
         }
 
+        // handle the request made by the client
         response_t* res;
         switch (client_req->command) {
             case WRITE:
                 res = handleWrite(client_req, client_socket_descriptor);
-                if (res != NULL) {
-                    int sendStatus = sendResponse(client_socket_descriptor, res);
-                    freeResponse(res);
-                }
+                sendResponse(client_socket_descriptor, res);
+                freeResponse(res);
                 break;
             case RM:
+                // todo
                 break;
             case GET:
+                // todo
                 break;
             case LS:
+                // todo: optional
                 break;
             default:
+                res = createResponse(false, "didn't receive valid command", 0);
+                sendResponse(client_socket_descriptor, res);
+                freeResponse(res);
                 break;
         }
+
         // close client socket now that we're finished with this client
         close(client_socket_descriptor);
     }
