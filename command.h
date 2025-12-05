bdb93472831ff17b6660abbb466f42c161a3b6fe
Estelita <24834264+estelita24601@users.noreply.github.com>
2025-12-04 19:02:00 -0800

    documentation

--- command.h
+++ command.h
@@ -27,16 +27,6 @@ typedef struct command {
 } command_t;
 
 /**
- * @brief - shorthand for strcmp(a,b) == 0
- *
- * @param a const char*
- * @param b const char*
- * @return true - if string a equals string b
- * @return false - otherwise
- */
-bool equals(const char* a, const char* b);
-
-/**
  * @brief - turn a string into our command_type enum
  *
  * @param str char*
@@ -45,28 +35,32 @@ bool equals(const char* a, const char* b);
 command_type strToCommandType(const char* str);
 
 /**
- * @brief
+ * @brief //todo
  *
- * @param str
- * @return char*
+ * Examples:
+ * "foo.txt" -> "data/foo.txt"
+ * "myfolder/foo.txt" -> "data/myfolder/foo.txt"
+ *
+ * @param str char* - original path given to us in the args
+ * @return char* - path after its been formatted to match PATH_FORMAT from config.h
  */
 char* formatPath(const char* str);
 
 /**
  * @brief Create a Command Struct object
  *
- * @param type command_type enum WRITE, GET, RM, LS, or UNKNOWN
+ * @param type command_type enum - WRITE, GET, RM, LS, or UNKNOWN
  * @param ip_address const char* - address for the server we should connect to
  * @param local const char* - path for the local file
- * @param remote
+ * @param remote const char* - path for the remote server file
  * @return command_t*
  */
 command_t* createCommandStruct(command_type type, const char* ip_address, const char* local, const char* remote);
 
 /**
- * @brief
+ * @brief free the command_t struct
  *
- * @param cmd
+ * @param cmd command_t*
  */
 void freeCommandStruct(command_t* cmd);
 
